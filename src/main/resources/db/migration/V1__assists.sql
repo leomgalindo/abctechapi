4563ad41670b68d962be03605a7dff9972eff350
leomgalindo <Aquy6246@l>
2022-10-05 09:37:39 -0300

    Ajuste nas queries

--- src/main/resources/db/migration/V1__assists.sql
+++ src/main/resources/db/migration/V1__assists.sql
@@ -1,13 +1,12 @@
-CREATE TABLE `assistances` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `description` varchar(300) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE assistances (
+  id bigint auto_increment primary key,
+  description varchar(300) not null,
+  name varchar(100) not null
+);
 
-INSERT INTO `abc_tech`.`assistances` (`description`, `name`) VALUES ('Troca de aparelho decodificador de sinal', 'Troca de aparelho');
-INSERT INTO `abc_tech`.`assistances` (`description`, `name`) VALUES ('Troca de cabo interno', 'Troca de cabo interno');
-INSERT INTO `abc_tech`.`assistances` (`description`, `name`) VALUES ('Substituição de fiação interna da resistência', 'Troca de fiação interna');
-INSERT INTO `abc_tech`.`assistances` (`description`, `name`) VALUES ('Reparo sem necessidade de compra de peças', 'Manutenção em fogão');
-INSERT INTO `abc_tech`.`assistances` (`description`, `name`) VALUES ('Reparo sem necessidade de compra de peças', 'Manutenção em geladeira');
-INSERT INTO `abc_tech`.`assistances` (`description`, `name`) VALUES ('Reparo sem necessidade de compra de peças', 'Manutenção em máquina de lavar');
+INSERT INTO assistances (description, name) VALUES ('Troca de aparelho decodificador de sinal', 'Troca de aparelho');
+INSERT INTO assistances (description, name) VALUES ('Troca de cabo interno', 'Troca de cabo interno');
+INSERT INTO assistances (description, name) VALUES ('Substituição de fiação interna da resistência', 'Troca de fiação interna');
+INSERT INTO assistances (description, name) VALUES ('Reparo sem necessidade de compra de peças', 'Manutenção em fogão');
+INSERT INTO assistances (description, name) VALUES ('Reparo sem necessidade de compra de peças', 'Manutenção em geladeira');
+INSERT INTO assistances (description, name) VALUES ('Reparo sem necessidade de compra de peças', 'Manutenção em máquina de lavar');
